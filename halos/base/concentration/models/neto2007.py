f44ed324a4852bba3940e7e4c6ee2bf67165806d
Milan Tol <milantol35@gmail.com>
2026-02-04 10:11:30 +0100

    .

---
+++ halos/base/concentration/models/neto2007.py
@@ -0,0 +1,15 @@
+from ..concentration import Concentration
+
+class ConcentrationNeto2007(Concentration):
+    
+    def _concentration(self, cosmo, M, z):#Model C1
+        """concentration-mass relation from Neto et al. (2007) 
+        from the aquarius simulation, related to M_200"""
+
+        c0 = 4.67
+        beta = -0.11
+        M_pivot = 1e14  # in M_sun/h
+
+        return c0 * (M / M_pivot)**beta * (1 + z)**-1
+    
+
---
+++ halos/base/profile/profile.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+class Profile(ABC):
+
+    @abstractmethod
+    def fourier(self, cosmo, k, M, z):
+        pass
