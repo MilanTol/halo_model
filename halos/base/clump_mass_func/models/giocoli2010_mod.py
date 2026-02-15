7698bdd151b0cb410beee1950212a62ba2d525bd
Milan Tol <milantol35@gmail.com>
2026-02-15 10:10:19 +0100

    .

--- halos/base/clump_mass_func/models/giocoli2010_mod.py
+++ halos/base/clump_mass_func/models/giocoli2010_mod.py
@@ -16,9 +16,58 @@ class ClumpMassFuncGiocoli2010_mod(ClumpMassFunc):
         alpha = -0.9          
         beta = 12.2715
         return M * (1+z)**0.5 * A * m**alpha * np.exp(-beta * (m / M)**3)
+
+    def R(self, m):
+        return (1 + self.m0/m)**self.beta
     
     def _cmf(self, m, M, z):
-        return (1 + self.m0/m)**self.beta * self.standard(m, M, z)
+        return self.R(m) * self.standard(m, M, z)
+    
+
+#have to recalculate this derivative
+
+# class delm0_ClumpMassFuncGiocoli2010_mod(ClumpMassFunc):
+
+#     def __init__(self, m0, beta):
+#         self.m0 = m0
+#         self.beta = beta
+
+#     def standard(self, m, M, z): 
+#         #This function is missing c/c_bar, 
+#         #this is only valid for  fixed relation concentration-mass.
+#         """Clump mass function from Giocoli et al. (2010)"""
+#         A = 9.33e-4 
+#         alpha = -0.9          
+#         beta = 12.2715
+#         return M * (1+z)**0.5 * A * m**alpha * np.exp(-beta * (m / M)**3)
+    
+#     def R(self, m):
+#         return (1 + self.m0/m)**self.beta
+    
+#     def _cmf(self, m, M, z):
+#         return 
+
+    
+class delbeta_ClumpMassFuncGiocoli2010_mod(ClumpMassFunc):
+
+    def __init__(self, m0, beta):
+        self.m0 = m0
+        self.beta = beta
+
+    def standard(self, m, M, z): 
+        #This function is missing c/c_bar, 
+        #this is only valid for  fixed relation concentration-mass.
+        """Clump mass function from Giocoli et al. (2010)"""
+        A = 9.33e-4 
+        alpha = -0.9          
+        beta = 12.2715
+        return M * (1+z)**0.5 * A * m**alpha * np.exp(-beta * (m / M)**3)
+    
+    def R(self, m):
+        return (1 + self.m0/m)**self.beta
+    
+    def _cmf(self, m, M, z):
+        return np.log(1 + self.m0/m) * self.R(m) * self.standard(m, M, z)
     
     
     
